d12a2c9b93e9ad3a54b4202683f7e6ef76f314f0
Romeao Jennings <romeao.jennings@gmail.com>
2019-06-19 13:59:23 -0700

    Add Specialty and associate it to Vet. Closes #27

--- pet-clinic-data/src/main/java/com/romeao/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/romeao/petclinic/models/Vet.java
@@ -1,11 +1,23 @@
 package com.romeao.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialization;
 
     public Vet() {
     }
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Specialty> getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(Set<Specialty> specialization) {
+        this.specialization = specialization;
     }
 }
