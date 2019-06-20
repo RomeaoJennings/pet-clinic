764fe029b7c3d69b57244ec5bde7da0151330593
Romeao Jennings <romeao.jennings@gmail.com>
2019-06-19 18:16:36 -0700

    Add stubs to broken links.  Closes #45

--- pet-clinic-data/src/main/java/com/romeao/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/romeao/petclinic/services/map/PetServiceMap.java
@@ -2,8 +2,22 @@ package com.romeao.petclinic.services.map;
 
 import com.romeao.petclinic.models.Pet;
 import com.romeao.petclinic.services.PetService;
+import com.romeao.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetServiceMap extends AbstractServiceMap<Pet> implements PetService {
+
+    private final PetTypeService petTypeService;
+
+    public PetServiceMap(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        super.save(object);
+        petTypeService.save(object.getPetType());
+        return object;
+    }
 }
--- pet-clinic-data/src/main/java/com/romeao/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/romeao/petclinic/services/map/VetServiceMap.java
@@ -1,9 +1,26 @@
 package com.romeao.petclinic.services.map;
 
+import com.romeao.petclinic.models.Specialty;
 import com.romeao.petclinic.models.Vet;
+import com.romeao.petclinic.services.SpecialtyService;
 import com.romeao.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VetServiceMap extends AbstractServiceMap<Vet> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        super.save(object);
+        for (Specialty specialty : object.getSpecialities()) {
+            specialtyService.save(specialty);
+        }
+        return object;
+    }
 }
