5b1f765511c014d0f5141ee9c132d6d406d133c1
Romeao Jennings <romeao.jennings@gmail.com>
2019-06-19 18:06:43 -0700

    Add bootstrap data for vet specialties.  Closes #34

--- pet-clinic-web/src/main/java/com/romeao/petclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/romeao/petclinic/bootstrap/DataInitializer.java
@@ -1,11 +1,7 @@
 package com.romeao.petclinic.bootstrap;
 
-import com.romeao.petclinic.models.Owner;
-import com.romeao.petclinic.models.Pet;
-import com.romeao.petclinic.models.PetType;
-import com.romeao.petclinic.models.Vet;
+import com.romeao.petclinic.models.*;
 import com.romeao.petclinic.services.OwnerService;
-import com.romeao.petclinic.services.PetTypeService;
 import com.romeao.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,19 +13,17 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
-    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
-        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        PetType dog = petTypeService.save(new PetType("Dog"));
-        PetType cat = petTypeService.save(new PetType("Cat"));
+        PetType dog = new PetType("Dog");
+        PetType cat = new PetType("Cat");
 
 
         Owner romeao = new Owner("Romeao", "Jennings", "18 Wakonda", "Dove Canyon", "(555) 555-5555");
@@ -41,8 +35,19 @@ public class DataInitializer implements CommandLineRunner {
         ownerService.save(caitlin);
         System.out.println("Loaded owners...");
 
-        vetService.save(new Vet("Liam", "Jennings"));
-        vetService.save(new Vet("Kennedy", "Jennings"));
+        Specialty radiology = new Specialty("Radiology");
+        Specialty dentistry = new Specialty("Dentistry");
+        Specialty surgery = new Specialty("Surgery");
+
+        Vet liam = new Vet("Liam", "Jennings");
+        liam.getSpecialities().add(radiology);
+        liam.getSpecialities().add(dentistry);
+        Vet kennedy = new Vet("Kennedy", "Jennings");
+        kennedy.getSpecialities().add(surgery);
+
+        vetService.save(liam);
+        vetService.save(kennedy);
+
         System.out.println("Loaded vets...");
     }
 }
