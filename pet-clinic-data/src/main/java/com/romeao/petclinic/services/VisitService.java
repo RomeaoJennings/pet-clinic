4bc4acc236f35f1a7e727117dead6b9daf4993bd
Romeao Jennings <romeao.jennings@gmail.com>
2019-06-23 07:06:32 -0700

    Create VisitService interface and VisitMapService implementation.  Closes #46

---
+++ pet-clinic-data/src/main/java/com/romeao/petclinic/services/VisitService.java
@@ -0,0 +1,5 @@
+package com.romeao.petclinic.services;
+
+import com.romeao.petclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> { }
---
+++ pet-clinic-data/src/main/java/com/romeao/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,6 @@
+package com.romeao.petclinic.services.map;
+
+import com.romeao.petclinic.models.Visit;
+import com.romeao.petclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit> implements VisitService { }
